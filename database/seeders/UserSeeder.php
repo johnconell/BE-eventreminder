3938132923591221f3081e10ba2851f6cde34506
nashvel <nash.backup21@gmail.com>
2024-11-23 17:07:22 +0800

    Update UserSeeder.php
    making it good code

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,22 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder; 
+use App\Models\User; 
+use Illuminate\Support\Facades\Hash; // Import Hash facade for password hashing
 
 class UserSeeder extends Seeder
 {
+
     public function run()
     {
         User::create([
-            'name' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('12345678'),
+            'name' => 'admin', // Name of the admin user
+            'email' => 'admin@gmail.com', // Email of the admin user
+            'password' => Hash::make('12345678'), // Securely hash the password for the admin user
         ]);
 
+        // Step 2: Generate 20 random users using the User factory
         User::factory()->count(20)->create();
     }
 }
-
-
