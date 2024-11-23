1f400ebfe12cd872a65d447290146774689b1f59
nashvel <nash.backup21@gmail.com>
2024-11-23 17:00:27 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function user(Request $request)
+    public function users(Request $request)
     {
         // Use the paginate method to paginate users, with a default of 10 users per page
         $users = User::paginate(10);
