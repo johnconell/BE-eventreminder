50b49bbba3ccc4f1bc75494e23a1543249f62fa6
nashvel <nash.backup21@gmail.com>
2024-11-23 17:02:04 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route; // Import the Route facade for defining routes
+use App\Http\Controllers\AuthController; // Import the AuthController for authentication-related routes
+use App\Http\Controllers\UserController; // Import the UserController for user-related routes
 
 
+// Route for user login
+// POST /login: This route handles user login requests
+// Calls the 'login' method in the AuthController
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/users', [UserController::class, 'user']);
+
+// Route for fetching the list of users
+// GET /users: This route retrieves a list of all users
+// Calls the 'users' method in the UserController
+Route::get('/users', [UserController::class, 'users']);
